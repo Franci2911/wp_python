11776babb69d1925c9f2cbeaf6a371c4fa96bb2b
Franci <miller29112018@gmail.com>
2026-04-29 23:29:09 -0300

    Atualização até o exercicios número 3

---
+++ exercicios.py
@@ -0,0 +1,53 @@
+'''
+1. Inteiros (int)
+Métodos e operações:
++ (adição)
+- (subtração)
+* (multiplicação)
+// (divisão inteira)
+% (módulo - resto da divisão)
+
+2. Números de Ponto Flutuante (float)
+Métodos e operações:
++ (adição)
+- (subtração)
+* (multiplicação)
+/ (divisão)
+** (potenciação)
+
+3. Strings (str)
+Métodos e operações:
+.upper() (converte para maiúsculas)
+.lower() (converte para minúsculas)
+.strip() (remove espaços em branco no início e no final)
+.split(sep) (divide a string em uma lista, utilizando sep como delimitador)
++ (concatenação de strings)
+
+4. Booleanos (bool)
+Operações lógicas:
+and (E lógico)
+or (OU lógico)
+not (NÃO lógico)
+== (igualdade)
+!= (diferença)'''
+
+
+# EXERCICIOS
+
+# 1 Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+#n1 = int(input("Digite o primeiro número:"))
+#n2 = int(input("Digite o segundo número: "))
+#retorno = (n1+n2)
+#print(f"A soma entre o primeiro número:{n1} e o segundo número:{n2} é igual a:{retorno}")
+
+
+# 2 Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+#n01 = int(input("Digite um numero:"))
+#retorno_n01 = (n01 % 5)
+#print(f"O resto da divisão é: {retorno_n01}")
+
+# 3 Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+num01 = int(input("Digite um número:"))
+num02 = float(input("Digite mais um número"))
+resultado03 = (num01 * num02)
+print(f"A multiplicação entre os números é: {resultado03}")
